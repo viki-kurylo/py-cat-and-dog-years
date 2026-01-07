9f7a3d2dd8b44ad1cbe6abcf90729b78e887d3ff
viki <v.yavdoschuk@gmail.com>
2026-01-07 12:36:06 +0100

    'solution'

--- app/test_main.py
+++ app/test_main.py
@@ -15,7 +15,13 @@ from app.main import get_human_age
         pytest.param(28, 29, [3, 3],
                      id="test_more_than_two_human_years_bottom_edge"),
         pytest.param(100, 100, [21, 17], id="test_more_than_two_human_years"),
+        pytest.param(-10, -10, [0, 0], id="test_negative_input"),
     ]
 )
 def test_can_convert(cat_age: int, dog_age: int, result: list) -> None:
     assert get_human_age(cat_age, dog_age) == result
+
+
+def test_incorrect_input() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("str", 3.14)
