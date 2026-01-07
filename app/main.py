085372f1fef3bbaa4d572a735ae36e8e1430a7cd
viki <v.yavdoschuk@gmail.com>
2026-01-07 12:04:36 +0100

    'solution'

--- app/main.py
+++ app/main.py
@@ -11,7 +11,8 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     elif cat_age < cat_second_year_gap + cat_other_years_gap:
         cat_human_years = 2
     else:
-        cat_human_years = (cat_age - cat_second_year_gap) // cat_other_years_gap + 2
+        cat_human_years = ((cat_age - cat_second_year_gap)
+                           // cat_other_years_gap + 2)
 
     dog_first_year_gap = 15
     dog_second_year_gap = 9 + dog_first_year_gap
@@ -24,6 +25,7 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     elif dog_age < dog_second_year_gap + dog_other_years_gap:
         dog_human_years = 2
     else:
-        dog_human_years = (dog_age - dog_second_year_gap) // dog_other_years_gap + 2
+        dog_human_years = ((dog_age - dog_second_year_gap)
+                           // dog_other_years_gap + 2)
 
     return [cat_human_years, dog_human_years]
--- app/test_main.py
+++ app/test_main.py
@@ -2,10 +2,11 @@ import pytest
 
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        pytest.param(0, 0, [0, 0], id = "test_zero_years"),
+        pytest.param(0, 0, [0, 0], id="test_zero_years"),
         pytest.param(15, 15, [1, 1], id="test_first_human_year_bottom_edge"),
         pytest.param(23, 23, [1, 1], id="test_first_human_years_upper_edge"),
         pytest.param(24, 24, [2, 2], id="test_second_human_years_bottom_edge"),
@@ -13,6 +14,5 @@ from app.main import get_human_age
         pytest.param(100, 100, [21, 17], id="test_more_than_two_human_years"),
     ]
 )
-def test_can_convert(cat_age: int, dog_age: int, result: list):
+def test_can_convert(cat_age: int, dog_age: int, result: list) -> None:
     assert get_human_age(cat_age, dog_age) == result
-
